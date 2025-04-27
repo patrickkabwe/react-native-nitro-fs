00e0664d8b18c782cc2c58241e6a8cf2e18361fb
Patrick Kabwe <patrickckabwe@gmail.com>
2025-04-27 15:38:52 +0200

    chore: rename temp.kt

--- android/src/main/java/com/nitrofs/NitroFSPackage.java
+++ android/src/main/java/com/nitrofs/NitroFSPackage.java
@@ -3,15 +3,15 @@ package com.nitrofs;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 
+import com.facebook.react.BaseReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
-import com.facebook.react.TurboReactPackage;
 import com.margelo.nitro.nitrofs.NitroFSOnLoad;
 
 import java.util.HashMap;
 
-public class NitroFSPackage extends TurboReactPackage {
+public class NitroFSPackage extends BaseReactPackage {
   @Nullable
   @Override
   public NativeModule getModule(@NonNull String name, @NonNull ReactApplicationContext reactContext) {
